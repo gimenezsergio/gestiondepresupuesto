d509285a40df1790426857783c98c23f641ab1cb
Sergio Gimenez <gimenezsergio123@gmail.com>
2022-10-11 20:10:38 -0300

    Agregamos funcionalidades basicas, calculos y graficos

--- program.js
+++ program.js
@@ -1,32 +1,70 @@
-// Años, meses. Fecha capturada desde el sistema, la hora del navegador
-// Ingresos, egresos con sus categorias 
-// Total por mes y total por categoria
-// 
-
-let calcularTotal = function () {
-    let egreso = parseInt(document.querySelector('#totalEgreso').innerText)
-    let ingreso = parseInt(document.querySelector('#totalIngreso').innerText)
-    let calculo = ingreso - egreso
-    document.querySelector('#total').innerText = calculo
-}
-
 let ingreso = function () {
     let ingreso = parseInt(document.querySelector('#ingreso').value)
-    let total = parseInt(document.querySelector('#totalIngreso').innerText)
-    let calculo = ingreso + total
-    document.querySelector('#totalIngreso').innerText = calculo
-    calcularTotal()
+    let ingresado = parseInt(document.querySelector('#ingresado').innerText)
+    let calculo = ingresado + ingreso
+    document.querySelector('#ingresado').innerText = calculo
+    balance()
 }
-
 
 let egreso = function () {
     let egreso = parseInt(document.querySelector('#egreso').value)
-    let total = parseInt(document.querySelector('#totalEgreso').innerText)
-    let calculo = total + egreso
-    document.querySelector('#totalEgreso').innerText = calculo
-    calcularTotal()
+    let egresado = parseInt(document.querySelector('#gastado').innerText)
+    let calculo = egresado + egreso
+    document.querySelector('#gastado').innerText = calculo
+    balance()
+}
+
+let balance = function () {
+    let ingresado = parseInt(document.querySelector('#ingresado').innerText)
+    let gastado = parseInt(document.querySelector('#gastado').innerText)
+    let calculo = ingresado - gastado
+    document.querySelector('#balance').innerText = calculo
+    grafico(ingresado, gastado)
 }
 
 
+const data = {
+    labels: [
+      'Gastado',
+      'Ingresado'
+    ],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [gastado, ingresado],
+      backgroundColor: [
+        'red',
+        'green'      ],
+      hoverOffset: 4
+    }]
+  };
 
+let miGrafico = new Chart('myChart', {
+    type: 'bar',
+    data: data
+  })
+
+let grafico = function(ingresado, gastado){
+    console.log(ingresado, gastado)
+    const data = {
+        labels: [
+          'Gastado',
+          'Ingresado'
+        ],
+        datasets: [{
+          label: 'My First Dataset',
+          data: [gastado, ingresado],
+          backgroundColor: [
+            'red',
+            'green'      ],
+          hoverOffset: 4
+        }]
+      };
+    
+      miGrafico.destroy()
+      miGrafico = new Chart('myChart', {
+        type: 'bar',
+        data: data
+      })
+
+}
 
